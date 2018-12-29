0fb325173316533631e73967feb040cf238222e6
wei41 <wei41@avaya.com>
2018-12-29 13:13:35 +0800

    change setting

--- src/main/java/org/gikyou/getsu/web/rest/UserResource.java
+++ src/main/java/org/gikyou/getsu/web/rest/UserResource.java
@@ -1,5 +1,16 @@
 package org.gikyou.getsu.web.rest;
 
+import static org.elasticsearch.index.query.QueryBuilders.queryStringQuery;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import javax.validation.Valid;
+
 import org.gikyou.getsu.config.Constants;
 import org.gikyou.getsu.domain.User;
 import org.gikyou.getsu.repository.UserRepository;
@@ -13,9 +24,6 @@ import org.gikyou.getsu.web.rest.errors.EmailAlreadyUsedException;
 import org.gikyou.getsu.web.rest.errors.LoginAlreadyUsedException;
 import org.gikyou.getsu.web.rest.util.HeaderUtil;
 import org.gikyou.getsu.web.rest.util.PaginationUtil;
-import com.codahale.metrics.annotation.Timed;
-import io.github.jhipster.web.util.ResponseUtil;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -24,16 +32,18 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+import com.codahale.metrics.annotation.Timed;
 
-import static org.elasticsearch.index.query.QueryBuilders.*;
+import io.github.jhipster.web.util.ResponseUtil;
 
 /**
  * REST controller for managing users.
@@ -210,4 +220,5 @@ public class UserResource {
             .stream(userSearchRepository.search(queryStringQuery(query)).spliterator(), false)
             .collect(Collectors.toList());
     }
+    
 }
